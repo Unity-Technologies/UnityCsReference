4c177ae2a9aedb58b6063eea1d675427c9fd123d
Unity Technologies <support@unity3d.com>
2019-10-11 18:05:30 +0000

    Unity 2019.2.9f1 C# reference source code

--- Editor/Mono/Utils/Paths.cs
+++ Editor/Mono/Utils/Paths.cs
@@ -329,11 +329,15 @@ namespace UnityEditor.Utils
         {
             var normalizedFileName = unsafeFileName.Trim().Normalize(NormalizationForm.FormD);
             var stringBuilder = new StringBuilder();
-            foreach (var c in normalizedFileName)
+
+            // We need to use a TextElementEmumerator in order to support UTF16 characters that may take up more than one char(case 1169358)
+            TextElementEnumerator charEnum = StringInfo.GetTextElementEnumerator(normalizedFileName);
+            while (charEnum.MoveNext())
             {
-                if (invalidFilenameChars.Contains(c))
+                var c = charEnum.GetTextElement();
+                if (c.Length == 1 && invalidFilenameChars.Contains(c[0]))
                     continue;
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c, 0);
                 if (unicodeCategory != UnicodeCategory.NonSpacingMark)
                     stringBuilder.Append(c);
             }
--- Modules/UIElements/Renderer/UIRChainBuilderImpl.cs
+++ Modules/UIElements/Renderer/UIRChainBuilderImpl.cs
@@ -1177,6 +1177,8 @@ namespace UnityEngine.UIElements.UIR.Implementation
 
             if (ve.renderChainData.firstClosingCommand != null)
             {
+                renderChain.OnRenderCommandRemoved(ve.renderChainData.firstClosingCommand, ve.renderChainData.lastClosingCommand);
+
                 var c = ve.renderChainData.firstClosingCommand;
                 while (c != ve.renderChainData.lastClosingCommand)
                 {
