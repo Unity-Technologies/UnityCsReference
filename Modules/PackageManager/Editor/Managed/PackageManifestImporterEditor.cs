c6ec7823afaf15f1c32437e9d54911a9f3081cf1
Unity Technologies <support@unity3d.com>
2020-02-26 13:41:18 +0000

    Unity 2019.3.3f1 C# reference source code

--- Modules/PackageManager/Editor/Managed/PackageManifestImporterEditor.cs
+++ Modules/PackageManager/Editor/Managed/PackageManifestImporterEditor.cs
@@ -18,7 +18,7 @@ namespace UnityEditor.PackageManager
     [CanEditMultipleObjects]
     internal class PackageManifestImporterEditor : AssetImporterEditor
     {
-        private enum PackageVisibility
+        enum PackageVisibility
         {
             DependsOnType,
             AlwaysHidden,
@@ -53,20 +53,20 @@ namespace UnityEditor.PackageManager
         private static readonly List<string> MinorUnityVersions = new List<string> { "1", "2", "3", "4" };
 
         [Serializable]
-        private class AdvancedSettings
+        class AdvancedSettings
         {
             public PackageVisibility visibility;
         }
 
         [Serializable]
-        private class PackageDependency
+        class PackageDependency
         {
             public string packageName;
             public string version;
         }
 
         [Serializable]
-        private class PackageUnityVersion
+        class PackageUnityVersion
         {
             public bool isEnable;
             public string major;
@@ -75,7 +75,7 @@ namespace UnityEditor.PackageManager
         }
 
         [Serializable]
-        private class PackageInformation
+        class PackageInformation
         {
             public string packageName;
             public string displayName;
@@ -87,7 +87,7 @@ namespace UnityEditor.PackageManager
         }
 
         [Serializable]
-        private class PackageManifestState : ScriptableObject
+        class PackageManifestState : ScriptableObject
         {
             public bool isValidFile;
             public PackageInformation info;
