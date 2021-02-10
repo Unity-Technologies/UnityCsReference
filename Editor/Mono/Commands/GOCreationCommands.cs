a4fc5011a29781c15e3ee181f19d80dc7fbab19f
Unity Technologies <support@unity3d.com>
2021-02-10 17:06:33 +0000

    Unity 2019.4.20f1 C# reference source code

--- Editor/Mono/Commands/GOCreationCommands.cs
+++ Editor/Mono/Commands/GOCreationCommands.cs
@@ -236,11 +236,7 @@ namespace UnityEditor
 
             go.GetComponent<Transform>().SetLocalEulerAngles(new Vector3(-90, 0, 0), RotationOrder.OrderZXY);
             var renderer = go.GetComponent<ParticleSystemRenderer>();
-            renderer.materials = new Material[]
-            {
-                Material.GetDefaultParticleMaterial(),
-                    null
-            };
+            renderer.material = Material.GetDefaultParticleMaterial();
             Place(go, parent);
         }
 
