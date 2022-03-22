968b4aaadfc9663f988f467d05e6790b2e6ab406
Unity Technologies <support@unity3d.com>
2022-03-22 18:47:13 +0000

    Unity 2019.4.37f1 C# reference source code

--- Editor/Mono/Audio/Effects/AudioMixerEffectPlugin.cs
+++ Editor/Mono/Audio/Effects/AudioMixerEffectPlugin.cs
@@ -4,25 +4,73 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
-using UnityEditor;
-using UnityEditor.Audio;
 
 namespace UnityEditor.Audio
 {
     public class AudioMixerEffectPlugin : IAudioEffectPlugin
     {
+        internal AudioMixerController m_Controller;
+        internal AudioMixerEffectController m_Effect;
+        internal MixerParameterDefinition[] m_ParamDefs;
+
+        static bool s_ParameterChangeUndoIsRecorded;
+        static bool s_ParameterChangeUndoGroupNameIsSet;
+
+        static readonly Dictionary<string, float> k_UpdatedParameterMap = new Dictionary<string, float>();
+
+        readonly HashSet<string> m_VerifiedParameters = new HashSet<string>();
+
         public override bool SetFloatParameter(string name, float value)
         {
+            if (!HasParameter(name))
+            {
+                return false;
+            }
+
+            float previousValue;
+
+            GetFloatParameter(name, out previousValue);
+
+            if (Mathf.Approximately(value, previousValue))
+            {
+                return true;
+            }
+
+            if (k_UpdatedParameterMap.Count == 0 && !s_ParameterChangeUndoIsRecorded)
+            {
+                Undo.RecordObject(m_Controller.TargetSnapshot, $"Change {name}");
+            }
+
+            k_UpdatedParameterMap[name] = value;
+
+            if (k_UpdatedParameterMap.Count > 1 && !s_ParameterChangeUndoGroupNameIsSet)
+            {
+                Undo.SetCurrentGroupName("Change Effect Parameters");
+                s_ParameterChangeUndoGroupNameIsSet = true;
+            }
+
             m_Effect.SetValueForParameter(m_Controller, m_Controller.TargetSnapshot, name, value);
+
             return true;
         }
 
         public override bool GetFloatParameter(string name, out float value)
         {
+            if (!HasParameter(name))
+            {
+                value = default(float);
+
+                return false;
+            }
+
+            if (k_UpdatedParameterMap.TryGetValue(name, out value))
+            {
+                return true;
+            }
+
             value = m_Effect.GetValueForParameter(m_Controller, m_Controller.TargetSnapshot, name);
+
             return true;
         }
 
@@ -38,6 +86,7 @@ namespace UnityEditor.Audio
                     return true;
                 }
             }
+
             minRange = 0.0f;
             maxRange = 1.0f;
             defaultValue = 0.5f;
@@ -60,8 +109,32 @@ namespace UnityEditor.Audio
             return AudioMixerController.EditingTargetSnapshot() && !m_Effect.bypass;
         }
 
-        internal AudioMixerController m_Controller;
-        internal AudioMixerEffectController m_Effect;
-        internal MixerParameterDefinition[] m_ParamDefs;
+        // Call this after each interaction that changes effect parameters.
+        // NOTE: It is assumed that parameter changes can only happen on a single effect at a time.
+        internal static void OnParameterChangesDone()
+        {
+            k_UpdatedParameterMap.Clear();
+            s_ParameterChangeUndoIsRecorded = false;
+            s_ParameterChangeUndoGroupNameIsSet = false;
+        }
+
+        bool HasParameter(string name)
+        {
+            if (m_VerifiedParameters.Contains(name))
+            {
+                return true;
+            }
+
+            for (var i = 0; i < m_ParamDefs.Length; ++i)
+            {
+                if (m_ParamDefs[i].name.Equals(name))
+                {
+                    m_VerifiedParameters.Add(name);
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
