44b9fbc8600fb7d3891362828f968de8b7feecfc
Unity Technologies <support@unity3d.com>
2023-06-21 14:44:26 +0000

    Unity 2023.1.1f1 C# reference source code

--- Runtime/Export/Burst/BurstAuthorizedExternalMethodAttribute.cs
+++ Runtime/Export/Burst/BurstAuthorizedExternalMethodAttribute.cs
@@ -3,9 +3,11 @@
 // https://unity3d.com/legal/licenses/Unity_Reference_Only_License
 
 using System;
+using UnityEngine.Scripting;
 
 namespace Unity.Burst
 {
+    [RequireAttributeUsages]
     [AttributeUsage(AttributeTargets.Method)]
     public class BurstAuthorizedExternalMethodAttribute : Attribute
     {
