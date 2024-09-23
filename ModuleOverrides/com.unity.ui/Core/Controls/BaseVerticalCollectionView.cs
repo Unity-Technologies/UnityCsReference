ae14473ede18d41930bad9294f0ff3346bd4a27a
Unity Technologies <support@unity3d.com>
2024-09-23 14:46:08 +0000

    Unity 2021.3.44f1 C# reference source code

--- ModuleOverrides/com.unity.ui/Core/Controls/BaseVerticalCollectionView.cs
+++ ModuleOverrides/com.unity.ui/Core/Controls/BaseVerticalCollectionView.cs
@@ -103,11 +103,54 @@ namespace UnityEngine.UIElements
         /// <summary>
         /// Called when an item is moved in the itemsSource.
         /// </summary>
+        /// <remarks>
+        /// This callback receives two IDs, the first being the ID being moved, the second being the destination ID.
+        /// In the case of a tree, the destination is the parent ID.
+        /// </remarks>
         public event Action<int, int> itemIndexChanged;
 
         /// <summary>
-        /// Called when the itemsSource is reassigned or changes size.
+        /// Raised when the data source of a vertical collection view is assigned a new reference or new type.
         /// </summary>
+        /// <remarks>
+        /// Use this event to handle changes to the vertical collection view's data source, ensuring the UI appropriately
+        /// reflects the new data. For example, if the data source changes from a list of characters to a list of items, you can
+        /// use this event to update the binding events so the UI fits the new type.
+        ///\\
+        ///\\
+        /// This event isn't raised if the selection or the size of the data source changes. For size changes, such as adding 
+        /// or removing an item from a list view, listen to the [[BaseListViewController.itemsSourceSizeChanged]] event. 
+        /// For selection changes, listen to the [[BaseVerticalCollectionView.selectionChanged]] event.
+        /// </remarks>
+        /// <example>
+        /// The following example illustrates that the @@itemsSourceChanged@@ event is only triggered when the [[BaseVerticalCollectionView.itemsSource|itemsSource]] property is changed, 
+        /// not when the contents of the data source are modified.
+        /// <code lang="cs">
+        /// <![CDATA[
+        /// var changedCount = 0;
+        /// var source = new List<string>();
+        /// var listView = new ListView();
+        /// 
+        /// listView.itemsSourceChanged += () => changedCount++;
+        /// 
+        /// // Changing the data source of the list view triggers the event.
+        /// listView.itemsSource = source;
+        /// 
+        /// // Adding an item to the source doesn't trigger itemsSourceChanged 
+        /// // because the data source reference remains the same.
+        /// source.Add("Hello World!"); 
+        /// 
+        /// // Adding an item to the ListView directly doesn't trigger itemsSourceChanged 
+        /// // because the data source reference remains the same.
+        /// listView.viewController.AddItems(1); 
+        /// 
+        /// Debug.Log(changedCount); // Outputs 1.
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// SA: [[BaseListViewController.itemsAdded]], [[BaseListViewController.itemsRemoved]]
+        /// </remarks>
         public event Action itemsSourceChanged;
 
         // [GR] We can get rid of this once the InternalTreeView is removed.
