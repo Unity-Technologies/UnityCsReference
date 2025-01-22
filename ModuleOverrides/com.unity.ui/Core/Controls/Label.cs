b5e8fd438d771adcbca7ca4580898211abcf0582
Unity Technologies <support@unity3d.com>
2025-01-22 09:38:12 +0000

    Unity 2021.3.48f1 C# reference source code

--- ModuleOverrides/com.unity.ui/Core/Controls/Label.cs
+++ ModuleOverrides/com.unity.ui/Core/Controls/Label.cs
@@ -28,13 +28,19 @@ namespace UnityEngine.UIElements
 
 
         /// <summary>
-        /// Constructs a label.
+        /// Constructs a Label with no initial text.
         /// </summary>
+        /// <remarks>
+        /// Use this constructor with no arguments to create an empty Label.
+        /// </remarks>
         public Label() : this(String.Empty) {}
         /// <summary>
-        /// Constructs a label.
+        /// Constructs a Label displaying the specified text.
         /// </summary>
-        /// <param name="text">The text to be displayed.</param>
+        /// <param name="text">The initial text to be displayed in the Label.</param>
+        /// <remarks>
+        /// Use the @@text@@ parameter to create a Label with the specified value as the initial text.
+        /// </remarks>
         public Label(string text)
         {
             AddToClassList(ussClassName);
