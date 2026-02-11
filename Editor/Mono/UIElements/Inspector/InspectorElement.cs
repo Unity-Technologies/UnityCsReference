56f07dcee67fd2c47871fc30e023ec36434e846c
Unity Technologies <support@unity3d.com>
2026-02-11 12:30:31 +0000

    Unity 2022.3.72f1 C# reference source code

--- Editor/Mono/UIElements/Inspector/InspectorElement.cs
+++ Editor/Mono/UIElements/Inspector/InspectorElement.cs
@@ -413,6 +413,8 @@ namespace UnityEditor.UIElements
             m_Editor = value;
             m_Rebind = true;
 
+            // We need to set the handler cache for any custom property drawers that may be created during Bind (UUM-132087).
+            ScriptAttributeUtility.propertyHandlerCache = value.propertyHandlerCache;
             this.Bind(m_Editor.serializedObject);
         }
 
